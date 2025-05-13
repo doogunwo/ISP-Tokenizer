973c016ff6b204079933b6bf736a05e32f1c75da
doogunwo <dgw0601@naver.com>
2025-05-13 18:09:28 +0900

    Stop tracking shell scripts and update .gitignore

--- tokenizer_process/test/build_test_Bpe.sh
+++
@@ -1,5 +0,0 @@
-g++ -std=c++17 -o test_bpe test_bpe.cpp \
-    -I../include \
-    -L/home/doogunwo/Desktop/tokenizers-0.20.3/tokenizers/tokenizers-cpp/build \
-    -ltokenizers_cpp -ltokenizers_c \
-    -lpthread -ldl
